e37584ae35deccf8f0a358c0fba2075b7a28caf6
ZIHAO LIU <92551109+yooohao@users.noreply.github.com>
2023-05-07 12:23:27 +0800

    Preparation steps

---
+++ src/test/java/com/example/shoppinguser/ShoppingUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppinguser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
